442f28cfff1cda2da692310871a9a562c6a7ac17
Hasanuzzaman92 <hasanuzzamanmethu@gmail.com>
2022-11-09 11:41:58 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -14,11 +14,17 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eli8ngd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const servicesCollection = client.db('a1Photography').collection('services')
+    }
+    finally{
+
+    }
+}
+
+run().catch(error => console.error(error))
 
 
 
